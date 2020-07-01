bc7fa595cd6921d282b64f10c12e0f1009af1989
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-07-01 13:11:34 -0700

    refactor allCards

--- client/src/components/pages/AllCards.js
+++ client/src/components/pages/AllCards.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import MemoryCard from "../ui/MemoryCard";
-import { orderBy } from "lodash";
 import axios from "axios";
 import { connect } from "react-redux";
 const userId = "42160c3e-7a5b-4fb9-b361-ac9598aca4e2";
@@ -11,80 +10,45 @@ class AllCards extends React.Component {
       super();
 
       this.state = {
-         allCards: [],
-         whatsfilteredBySearch: [],
-         whatSearched: "",
-         whatOrderRendered: [],
-         cardsRendered: [],
+         order: "memory_cards.created_at%20DESC",
+         memoryCards: [],
+         searchTerm: "",
       };
    }
 
    componentDidMount() {
+      this.setMemoryCards();
+   }
+
+   setOrder(e) {
+      const newOrder = e.target.value;
+      this.setState({ order: newOrder }, () => {
+         this.setMemoryCards();
+      });
+   }
+
+   setSearchTerm() {
+      const searchInput = document.getElementById("search-this-box").value;
+      this.setState({ searchTerm: searchInput }, () => {
+         this.setMemoryCards();
+      });
+   }
+   setMemoryCards() {
       axios
          .get(
-            `/api/v1/memory-cards?userId=${userId}&order=memory_cards.created_at%20DESC`
+            `/api/v1/memory-cards?userId=${userId}&searchTerm=${this.state.searchTerm}&order=${this.state.order}`
          )
          .then((res) => {
             // handle success
             console.log(res);
-            const memoryCards = res.data;
             this.setState({
-               allCards: memoryCards,
-               whatsfilteredBySearch: memoryCards,
-               whatOrderRendered: ["createdAt", "desc"],
-               cardsRendered: orderBy([...memoryCards], "createdAt", "desc"),
+               memoryCards: res.data,
             });
          })
          .catch((error) => {
             // handle error
             console.log(error);
          });
-   }
-
-   async filterCardsbySearch() {
-      const arrOfMatches = this.state.allCards.filter((card) => {
-         return (
-            card.answer
-               .toLowerCase()
-               .indexOf(this.state.whatSearched.toLowerCase()) > -1 ||
-            card.imagery
-               .toLowerCase()
-               .indexOf(this.state.whatSearched.toLowerCase()) > -1
-         );
-      });
-      this.setState({ whatsfilteredBySearch: arrOfMatches });
-   }
-
-   async filterCardsBySelect(e) {
-      //const arrOfSelected = [...this.state.cardsRendered];
-      if (e.target.value === "Most recent") {
-         this.setState({ whatOrderRendered: ["createdAt", "desc"] });
-      } else if (e.target.value === "Oldest") {
-         this.setState({ whatOrderRendered: ["createdAt", "asc"] });
-      } else if (e.target.value === "Hardest") {
-         this.setState({
-            whatOrderRendered: [
-               ["totalSuccessfulAttempts", "createdAt"],
-               ["asc", "asc"],
-            ],
-         });
-      } else if (e.target.value === "Easiest") {
-         this.setState({
-            whatOrderRendered: [
-               ["totalSuccessfulAttempts", "createdAt"],
-               ["desc", "desc"],
-            ],
-         });
-      }
-   }
-
-   async updateFilterState() {
-      this.setState({
-         cardsRendered: orderBy(
-            [...this.state.whatsfilteredBySearch],
-            ...this.state.whatOrderRendered
-         ),
-      });
    }
 
    render() {
@@ -98,19 +62,14 @@ class AllCards extends React.Component {
                      type="search"
                      placeholder="Search"
                      aria-label="Search"
-                     onChange={(e) => {
-                        this.setState({ whatSearched: e.target.value });
-                        console.log(this.state.whatSearched);
-                     }}
                   />
                </div>
                <div className="col-4">
                   <button
                      id="use-this-btn-to-search"
                      className="btn btn-sm btn-primary text-white btn-block"
-                     onClick={async () => {
-                        await this.filterCardsbySearch();
-                        this.updateFilterState();
+                     onClick={() => {
+                        this.setSearchTerm();
                      }}
                   >
                      Search
@@ -122,20 +81,23 @@ class AllCards extends React.Component {
                <select
                   id="sort"
                   className="form-control border-primary col-8 float-right w-50 pt-0 "
-                  onChange={async (e) => {
-                     await this.filterCardsBySelect(e);
-                     this.updateFilterState();
+                  onChange={(e) => {
+                     this.setOrder(e);
                   }}
                >
-                  <option className="" defaultValue>
+                  <option className="" value="memory_cards.created_at%20DESC">
                      Most recent
                   </option>
-                  <option>Oldest</option>
-                  <option>Hardest</option>
-                  <option>Easiest</option>
+                  <option value="memory_cards.created_at%20ASC">Oldest</option>
+                  <option value="memory_cards.total_successful_attempts%20ASC,%20memory_cards.created_at%20ASC">
+                     Hardest
+                  </option>
+                  <option value="memory_cards.total_successful_attempts%20DESC,%20memory_cards.created_at%20DESC">
+                     Easiest
+                  </option>
                </select>
             </div>
-            {this.state.cardsRendered.map((memoryCard) => {
+            {this.state.memoryCards.map((memoryCard) => {
                return <MemoryCard card={memoryCard} key={memoryCard.id} />;
             })}
          </AppTemplate>
