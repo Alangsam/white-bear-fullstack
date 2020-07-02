d9a5add708c0a775166ceb7c46a85c75bae5d322
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-07-02 10:32:08 -0700

    add build

---
+++ client/build/precache-manifest.1f3497849dbad2aefe8691c5b36c07fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f2b499b6eff5db0bb24972259b657f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a6c97be474bca2ef00",
+    "url": "/static/css/main.71e26630.chunk.css"
+  },
+  {
+    "revision": "3dc0a3faa271df403496",
+    "url": "/static/js/2.7daddec0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7daddec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59a6c97be474bca2ef00",
+    "url": "/static/js/main.92ea8810.chunk.js"
+  },
+  {
+    "revision": "5619d3a5f1ad0b967891",
+    "url": "/static/js/runtime-main.078e1420.js"
+  },
+  {
+    "revision": "4c17826e1261b79e8a3a1202c0987ec8",
+    "url": "/static/media/background-landing.4c17826e.jpg"
+  },
+  {
+    "revision": "52ec66aa73668ee82efd75ab2f085459",
+    "url": "/static/media/edit.52ec66aa.svg"
+  },
+  {
+    "revision": "b1060c5b0ff9dad3f12630787bf72f54",
+    "url": "/static/media/logo-app.b1060c5b.svg"
+  },
+  {
+    "revision": "0ed185aee6e1b43d6e03b75b3a8af000",
+    "url": "/static/media/save.0ed185ae.svg"
+  },
+  {
+    "revision": "3d4b2818898d9983d090486a605e3a72",
+    "url": "/static/media/thumbs-up.3d4b2818.svg"
+  }
+]);
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(cors());
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
+app.use("/", require("./client/build"));
 
 const port = process.env.PORT || 3012;
 app.listen(port, () =>
