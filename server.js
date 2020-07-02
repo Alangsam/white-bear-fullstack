f587b1de5a599eb20d9a5a80e0c4eb654ccd3804
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-07-02 10:48:15 -0700

    get ready for heroku

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-var cors = require("cors");
-
-app.use(cors());
+const path = require("path");
 
 app.use("/api/v1/users", require("./api/v1/users"));
 app.use("/api/v1/memory-cards", require("./api/v1/memory-cards"));
-app.use("/", require("./client/build"));
+
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 3012;
 app.listen(port, () =>
