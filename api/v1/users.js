3ca77477e01c879754637906576318d2241e145f
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-07-06 12:06:11 -0700

    deleted selectuser

--- api/v1/users.js
+++ api/v1/users.js
@@ -1,25 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../db");
-const selectUser = require("../../queries/selectUser");
-const { toJson, toSafeParse, toHash } = require("../../utils/helpers");
 const insertUser = require("../../queries/insertUser");
-
-//@route        GET api/v1/users
-//@desc         GET a valid user via email and password
-//@access       Public
-router.get("/", (req, res) => {
-   db.query(selectUser("blah@email.com", "replace_me"))
-      .then((dbRes) => {
-         const users = toSafeParse(toJson(dbRes));
-         //console.log(users);
-         res.json(users);
-      })
-      .catch((err) => {
-         console.log(err);
-         res.status(400).json(err);
-      });
-});
 
 //@route        POST api/v1/users
 //@desc         Create a new user
